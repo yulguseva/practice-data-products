849dbdf12d815544d4c77b07a62f813d994fac58
yulia.guseva@itv.com <yuliguse@127.0.0.1>
2024-11-26 14:03:48 +0000

    added functions generate_past_dates and test_generate_past_dates

--- data-transformation/practice_data_product/extension/lag_features_extensions.py
+++ data-transformation/practice_data_product/extension/lag_features_extensions.py
@@ -4,7 +4,9 @@ from pyspark.sql.functions import(
     min,
     max,
     col,
-    round as round_col
+    round as round_col,
+    date_sub,
+    expr
 )
 
 
@@ -27,4 +29,30 @@ def calculate_rolling_average(date_column: str, target_column: str, window_size_
         )
         )
     return transform_df
+
+def generate_past_dates(date_column: str, week_lag: int) -> DataFrame:
+    """
+    generates new column in the given DataFrame that is week_lag weeks before dates in the date_column
+    """
+    day_lag = week_lag * 7
+    new_column_name = f"date_{week_lag}weeks_ago"
+    
+    return lambda df: df.withColumn(
+                            "breakdatetimeiso", col("breakdatetimeiso").cast("timestamp")
+                        ).withColumn(
+                            new_column_name, col("breakdatetimeiso") - expr(f"INTERVAL {day_lag} DAYS")
+                        )
+    
+
+
+def calculate_lag_features(date_column: str, target_column: str, window_size_weeks: int, category_column: str)  -> DataFrame:
+    """
+    for a given DataFrame generate rolling average for dates week_lag before dates in the date_column 
+    with window equaling window_size_weeks
+
+    horizon  = week_lag
+    window = window_size_weeks
+    filter = category_column
+    """
+    pass
 
--- data-transformation/tests/extensions_test/lag_features_extensions_test.py
+++ data-transformation/tests/extensions_test/lag_features_extensions_test.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import pytest
-from practice_data_product.extension.lag_features_extensions import calculate_rolling_average
+from practice_data_product.extension.lag_features_extensions import calculate_rolling_average, generate_past_dates
+
 from pyspark.sql.types import StringType, LongType, DoubleType, StructField, StructType, TimestampType
 
 
@@ -47,3 +48,51 @@ def test_calculate_rolling_average(spark_session):
 
     assert actual_df.collect() == expected_df.collect()
     assert actual_df.schema == expected_df.schema
+
+
+@pytest.mark.usefixtures("spark_session")
+def test_generate_past_dates(spark_session):
+    #   given
+    data = [
+        (datetime.strptime('2022-11-01 12:08:00', "%Y-%m-%d %H:%M:%S"), 'A', 1),
+        (datetime.strptime('2022-12-05 12:08:00', "%Y-%m-%d %H:%M:%S"), 'A', 90),
+        (datetime.strptime('2022-12-06 12:08:00', "%Y-%m-%d %H:%M:%S"), 'A', 91),
+        (datetime.strptime('2022-12-07 12:08:00', "%Y-%m-%d %H:%M:%S"), 'A', 92),
+    ]
+    schema = StructType([StructField("breakdatetimeiso", TimestampType(), True),  
+              StructField("showname", StringType(), True),
+              StructField("consolidatedimpact", LongType(), True)     
+    ])
+
+    df = spark_session.createDataFrame(data, schema)
+
+    # when
+    actual_df = df.transform(generate_past_dates("breakdatetimeiso", 3))
+    #actual_df = generate_past_dates(df, "breakdatetimeiso", 3)
+    #actual_df = df.transform(calculate_rolling_average("breakdatetimeiso", "consolidatedimpact", 1, "showname"))
+    # actual_df.show()
+
+    # then
+    expected_data = [
+        (datetime.strptime('2022-11-01 12:08:00', "%Y-%m-%d %H:%M:%S"), 'A', 1, datetime.strptime('2022-10-11 12:08:00', "%Y-%m-%d %H:%M:%S")),
+        (datetime.strptime('2022-12-05 12:08:00', "%Y-%m-%d %H:%M:%S"), 'A', 90, datetime.strptime('2022-11-14 12:08:00', "%Y-%m-%d %H:%M:%S")),
+        (datetime.strptime('2022-12-06 12:08:00', "%Y-%m-%d %H:%M:%S"), 'A', 91, datetime.strptime('2022-11-15 12:08:00', "%Y-%m-%d %H:%M:%S")),
+        (datetime.strptime('2022-12-07 12:08:00', "%Y-%m-%d %H:%M:%S"), 'A', 92, datetime.strptime('2022-11-16 12:08:00', "%Y-%m-%d %H:%M:%S"))
+    ]
+
+    expected_schema = StructType([StructField("breakdatetimeiso", TimestampType(), True),  
+              StructField("showname", StringType(), True),
+              StructField("consolidatedimpact", LongType(), True),
+              StructField("date_3weeks_ago", TimestampType(), True),  
+    ])
+
+    expected_df = spark_session.createDataFrame(data = expected_data, schema =expected_schema)
+
+    assert actual_df.collect() == expected_df.collect()
+    assert actual_df.schema == expected_df.schema
+
+
+
+@pytest.mark.usefixtures("spark_session")
+def test_calculate_lag_features(spark_session):
+    pass
